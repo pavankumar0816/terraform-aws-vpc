b42f55c47d5793936b38cf1c9a3203b57cfe3bb5
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-03-03 15:42:45 +0530

    configured  route table associations

--- main.tf
+++ main.tf
@@ -158,3 +158,25 @@ resource "aws_route" "database" {
    destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id = aws_nat_gateway.main.id
 }
+
+
+# public route table association
+resource "aws_route_table_association" "public" {
+     count = length(var.public_subnet_cidrs)
+     subnet_id = aws_subnet.public[count.index].id
+     route_table_id = aws_route_table.public.id
+}
+
+# private route table association
+resource "aws_route_table_association" "private" {
+     count = length(var.private_subnet_cidrs)
+     subnet_id = aws_subnet.private[count.index].id
+     route_table_id = aws_route_table.private.id
+}
+
+# database route table association
+resource "aws_route_table_association" "database" {
+     count = length(var.database_subnet_cidrs)
+     subnet_id = aws_subnet.database[count.index].id
+     route_table_id = aws_route_table.database.id
+}
