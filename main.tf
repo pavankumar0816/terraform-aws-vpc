26acac088aaf88f23e87db033b7595ce88c995c8
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-03-03 15:25:16 +0530

    created route tables

--- main.tf
+++ main.tf
@@ -105,3 +105,56 @@ resource "aws_route_table" "database" {
   )
 }
 
+# public route
+# it will create one entry with 0.0.0.0/0 in public route table which points to the internet gateway
+resource "aws_route" "public" {
+   route_table_id = aws_route_table.public.id
+   destination_cidr_block = "0.0.0.0/0"
+   gateway_id = aws_internet_gateway.main.id
+}
+
+# Elastic-IP
+resource "aws_eip" "nat" {
+    domain   = "vpc"
+    tags = merge(
+        local.common_tags,
+        #roboshop-dev-nat
+        {
+          Name = "${var.project}-${var.environment}-nat"
+        },
+        var.eip_tags
+    )
+}
+
+# Nat Gateway
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id # we are creating this in us-east-1a AZ
+
+  tags = merge(
+      local.common_tags,
+      #roboshop-dev
+      {
+        Name = "${var.project}-${var.environment}"
+      },
+      var.nat_gateway_tags
+    )
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.main]
+}
+
+# private route
+resource "aws_route" "private" {
+   route_table_id = aws_route_table.private.id
+   destination_cidr_block = "0.0.0.0/0"
+   nat_gateway_id = aws_nat_gateway.main.id
+}
+
+# database route
+resource "aws_route" "database" {
+   route_table_id = aws_route_table.database.id
+   destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.main.id
+}
